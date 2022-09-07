a083312091a42da7ada99320381d4a5d3c91e6a5
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-09-07 14:04:24 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang = "en">
+  <body>
+    <h1> Hello World </h1>
+    <form action="handlepost.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+    </html>
+  </body>

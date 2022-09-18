80595431e87175a5a89311cc3022333d093564e3
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-09-18 18:20:19 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,10 +1,9 @@
 <html>
-  <body>
-    
-    Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+  <head>
+    <?php require_once('header.php');?>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
-<nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,5 +19,12 @@ Your email address is: <?php echo $_POST["email"]; ?>
     </div>
   </div>
 </nav>
+  </head>
+  <body>
+    
+    Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+
 </body>
 </html>

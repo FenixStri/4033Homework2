4862e110ce140a9e4d99eec3e7d4a119366ddb12
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-09-18 18:42:38 -0500

    Update header.php

--- header.php
+++ header.php
@@ -42,18 +42,7 @@
     </div>
   </div>
 </nav>
-    <div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
-  </div>
+   
   </head>
   <body>
    

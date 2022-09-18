16ffe65417b4da6c31ac714f935a430803a8fe2f
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-09-18 18:31:25 -0500

    Update header.php

--- header.php
+++ header.php
@@ -45,6 +45,8 @@
     </div>
   </div>
 </nav>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
     <div class="container text-center">
   <div class="row align-items-start">
     <div class="col">

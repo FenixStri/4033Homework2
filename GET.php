719b4857fe88e1056ee75228665656daa14bea7f
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-09-18 20:07:55 -0500

    Update GET.php

--- GET.php
+++ GET.php
@@ -6,7 +6,7 @@
     <div class="container text-center">
   <div class="row align-items-start">
     <div class="col">
-      GET
+      
  Welcome to the GET page <?php echo $_GET["name"]; ?><br>
  Your email address is: <?php echo $_GET["email"]; ?>
     </div>
@@ -14,7 +14,16 @@
       One of three columns
     </div>
     <div class="col">
-      One of three columns
+      Information
+            <div class="card">
+  <img src="wattter.png" class="card-img-top" alt="Ocean">
+
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     </div>
   </div>
   

6a824dd8f7b4dd246602a0d9ca91459b92c317d7
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-09-18 19:20:56 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -7,6 +7,9 @@
     
     Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
+    
+    Welcome GET <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 
 
 </body>

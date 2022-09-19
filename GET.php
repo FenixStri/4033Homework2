859e1f4d1fb153ead30f16f095da13cdcc438374
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-09-18 19:59:30 -0500

    Create GET.php

---
+++ GET.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+     <?php require_once('header.php');?>
+  </head>
+  <body>
+    <div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+      GET
+ Welcome to the GET page <?php echo $_GET["name"]; ?><br>
+ Your email address is: <?php echo $_GET["email"]; ?>
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+  </div>
+  
+      </body>
+</html>
